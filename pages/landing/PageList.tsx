b6e24c843be76072c5ed31fa702c1a85fdc7c51d
李茂峰 <limaofeng@msn.com>
2022-05-05 20:41:07 +0800

    chore: 动态获取协议

--- pages/landing/PageList.tsx
+++ pages/landing/PageList.tsx
@@ -68,7 +68,7 @@ function Actions({ data, history, onDelete, onShowQRCode }: ActionsProps) {
       } else if (event.key == 'delete') {
         handleDelete(data);
       } else if (event.key == 'preview') {
-        onShowQRCode('https:' + process.env.MOBILE_URL + '/lps/' + data.id);
+        onShowQRCode(location.protocol + process.env.MOBILE_URL + '/lps/' + data.id);
       }
     },
     [data, handleDelete, history, onShowQRCode],
